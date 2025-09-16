851d6588701af0ef07beb6a595b1461bd974b81c
Mohd Sahil Ansari <sahilansarilogin@gmail.com>
2025-09-16 13:48:25 +0530

    second commit

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # sqlalchemy is an  ORM mapper it provides facilty to  make changes in database through python 
@@ -58,4 +59,5 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
 
-    app.run(debug=True,port=5000)
+    # app.run(debug=True,port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
